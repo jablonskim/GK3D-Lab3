53058cddd670e06c68cca8ce50ea2a60b13a87b3
Mateusz Jabłoński <mjablonski92@gmail.com>
2015-12-24 17:00:38 +0100

    Fix

--- GK3D/GK3D/Shaders/SSAOBlurFragmentShader.glsl
+++ GK3D/GK3D/Shaders/SSAOBlurFragmentShader.glsl
@@ -2,9 +2,7 @@
 
 in vec2 tex_coord;
 
-// TODO
-//out float blured_occlusion;
-out vec4 color;
+out float blured_occlusion;
 
 uniform sampler2D occlusion;
 
@@ -14,18 +12,16 @@ void main()
 {
 	vec2 texel_size = 1.0 / vec2(textureSize(occlusion, 0));
 
-	float result = 0.0;
+	blured_occlusion = 0.0;
 
 	for (int x = 0; x < blur_size; ++x) 
 	{
 		for (int y = 0; y < blur_size; ++y) 
 		{
 			vec2 offset = (vec2(-float(blur_size / 2)) + vec2(float(x), float(y))) * texel_size;
-			result += texture(occlusion, tex_coord + offset).r;
+			blured_occlusion += texture(occlusion, tex_coord + offset).r;
 		}
 	}
 
-	result /= float(blur_size * blur_size);
-	//blured_occlusion = result;
-	color = vec4(result, result, result, 1.0f);
+	blured_occlusion /= float(blur_size * blur_size);
 }
